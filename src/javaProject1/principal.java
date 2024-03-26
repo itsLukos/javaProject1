7ff0545b9d61dcc8f9215f679b4baa0b84e80387
Luka <107578293+itsLukos@users.noreply.github.com>
2024-03-26 07:50:15 +0100

    Initial commit

---
+++ src/javaProject1/principal.java
@@ -0,0 +1,33 @@
+package javaProject1;
+
+public class principal {
+	private String nombre;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hola mundo");
+//		int i = 9;
+//		while (i>0) {
+//			System.out.println (i);
+//			i--;
+//		}
+		Bjjko a = new Bjjko();
+		// System.out.println(a.numeros());
+		 a.ej1();
+		//a.ej2();
+		// a.ej3();
+		// a.ej4();
+		// a.ej5();
+		// a.ej6();
+		// a.ej7();
+		// a.ej8();
+		// a.ej9();
+		// a.ej10();
+		// a.ej11();
+		// a.ej12();
+		// a.ej13();
+		// a.ej14();
+		// a.ej15();
+	}
+
+}
